979a8165d39caf7e85d8b4ba23fcffe2b25ddf98
Ian Stewart <32638123+igstewart3@users.noreply.github.com>
2025-05-01 10:42:06 +1200

    [MP-2207] Update Android SDK (#13)
    * [MP-2207] Update Android SDK
    
    * [MP-2207] Update version to 5.0.0 for release

--- AndroidUnityWrapper~/build.gradle.kts
+++ AndroidUnityWrapper~/build.gradle.kts
@@ -1,13 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    val kotlinVersion by extra("1.9.24")
+    val kotlinVersion by extra("2.1.20")
     repositories {
         mavenCentral()
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.6.1")
+        classpath("com.android.tools.build:gradle:8.9.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 
         // NOTE: Do not place your application dependencies here; they belong
